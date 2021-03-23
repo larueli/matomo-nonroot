fd5d047329ac2682d8d9629fc6148822995f4e69
Ivann LARUELLE <ivann+git@laruelle.me>
2021-03-23 11:23:34 +0100

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-if [ ! -f /var/www/html/config/global.php ]; then
+if [ ! $(ls -A /var/www/html) ]; then
     cd /tmp
     unzip matomo-${MATOMO_VERSION}.zip
-    mv -f matomo/* /var/www/html/
+    mv -f matomo/config/* /var/www/html/config/
+fi
+if [ ! $(ls -A /var/www/html/plugins) ]; then
+    cd /tmp
+    unzip -o matomo-${MATOMO_VERSION}.zip
+    mv -f matomo/plugins/* /var/www/html/plugins/
 fi
