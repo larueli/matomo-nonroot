15a30e8c34ed57f2b0d46b8593d34c8ba8ea56a3
Ivann LARUELLE <ivann+git@laruelle.me>
2022-04-14 08:49:30 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 
 cd /tmp
 unzip -o matomo-${MATOMO_VERSION}.zip
-mv -f matomo/plugins/* /var/www/html/plugins/
+rsync -a matomo/plugins/ /var/www/html/plugins/
 
-if [ ! $(ls -A /var/www/html/config) ]; then
+if [ ! "$(ls -A /var/www/html/config)" ]; then
     mv -f matomo/config/* /var/www/html/config/
 fi
