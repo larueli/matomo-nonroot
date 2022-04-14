45985591e11803410ebc433c46d1ea0f2e39f260
Ivann LARUELLE <ivann+git@laruelle.me>
2022-04-14 08:45:03 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
+
+cd /tmp
+unzip -o matomo-${MATOMO_VERSION}.zip
+mv -f matomo/plugins/* /var/www/html/plugins/
+
 if [ ! $(ls -A /var/www/html/config) ]; then
-    cd /tmp
-    unzip matomo-${MATOMO_VERSION}.zip
     mv -f matomo/config/* /var/www/html/config/
-fi
-if [ ! $(ls -A /var/www/html/plugins) ]; then
-    cd /tmp
-    unzip -o matomo-${MATOMO_VERSION}.zip
-    mv -f matomo/plugins/* /var/www/html/plugins/
 fi
